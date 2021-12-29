c45e85c3e536bea85c93a401ab7751edbfed2d73
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-12-29 14:17:20 -0800

    Revert "used dictionary api"
    This reverts commit 2b4bec958ed590231dec499d93d4a126d2023945.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Search from "./search"; 
 
-
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Dictionary</h1>
         <Search/>
-      
-   
+        <p>
+          Definition
+        </p>
         <a
           className="App-link"
           href="https://github.com/CosmicBee/dictionary-react"
@@ -21,16 +21,8 @@ export default function App() {
           GitHub
         </a>
       </header>
-    
     </div>
   );
 }
 
-
-
-
-
-
-
-
-
+export default App;
