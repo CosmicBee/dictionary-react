044622e380bfa7e2fad96d9af692dc42da3d3bf3
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-12-29 14:48:54 -0800

    minus meaning
    This reverts commit

--- src/search.js
+++ src/search.js
@@ -1,24 +1,27 @@
 import React,{useState} from "react";
-import "./search.css";
 import axios from "axios";
+import Results from "./Results";
+import "./search.css";
+
+
 
 export default function Search() {
 let [keyword, setkeyword] = useState("");
+let[results, setResults] = useState(null);
 
 
 function handleResponse(response){
-  console.log(response.data[0]);
+  setResults(response.data[0]);
 };
 
   function find(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
 
 
     //Api Documentation https://dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/$(keyword)`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
-  
   }
 
   function handleKeywordChange(event) {
@@ -30,6 +33,7 @@ function handleResponse(response){
       <form onSubmit={find}>
         <input type="search" onChange= {handleKeywordChange} />
       </form>
+      <Results results = {results}/>
     </div>
   );
  }
