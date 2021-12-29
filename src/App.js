2b4bec958ed590231dec499d93d4a126d2023945
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-12-28 21:09:46 -0800

    used dictionary api
    search spits out the word noun and verb

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Search from "./search"; 
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Dictionary</h1>
         <Search/>
-        <p>
-          Definition
-        </p>
+      
+   
         <a
           className="App-link"
           href="https://github.com/CosmicBee/dictionary-react"
@@ -21,8 +21,16 @@ function App() {
           GitHub
         </a>
       </header>
+    
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
